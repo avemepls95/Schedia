40f539d7c0c82920b1fe57848c5ee95a7c7915a1
avemepls <vmpls9513@gmail.com>
2026-02-08 20:06:05 +0200

    Add logging

--- src/Schedia.Web.Blazor/Program.cs
+++ src/Schedia.Web.Blazor/Program.cs
@@ -1,4 +1,6 @@
 using Avemepls.Infrastructure;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Serilog.Sinks;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.IdentityModel.Logging;
 using Schedia.Web.Base;
@@ -6,75 +8,103 @@ using Schedia.Web.Blazor;
 using Schedia.Web.Blazor.Components;
 using Schedia.Web.Blazor.Endpoints;
 using Schedia.Web.Core.Blazor.BlazorAssembliesRegistry;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-var services = builder.Services;
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var configuration = builder.Configuration;
-
-services
-    .AddMemoryCache()
-    .AddInfrastructureServices();
-
-builder.Services.AddSchediaBase(configuration);
-builder.AddBlazor();
-
-var app = builder
-    .Build()
-    .InitializeData();
-
-app.UseForwardedHeaders();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
-}
+    var builder = WebApplication.CreateBuilder(args);
+    var services = builder.Services;
 
-if (!app.Environment.IsDevelopment())
+    var elasticsearchNodes = builder.Configuration
+        .GetSection("Elasticsearch:Nodes")
+        .Get<string[]>() ?? ["http://localhost:9200"];
+
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .WriteTo.Elasticsearch(
+            elasticsearchNodes.Select(n => new Uri(n)).ToArray(),
+            opts => opts.BootstrapMethod = BootstrapMethod.Silent));
+
+    // Add services to the container.
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    var configuration = builder.Configuration;
+
+    services
+        .AddMemoryCache()
+        .AddInfrastructureServices();
+
+    builder.Services.AddSchediaBase(configuration);
+    builder.AddBlazor();
+
+    var app = builder
+        .Build()
+        .InitializeData();
+
+    app.UseSerilogRequestLogging();
+    app.UseForwardedHeaders();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    if (!app.Environment.IsDevelopment())
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+        app.UseHttpsRedirection();
+    }
+    else
+    {
+        IdentityModelEventSource.ShowPII = true;
+    }
+
+    app.UseDeveloperExceptionPage();
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    var localizationOptions = new RequestLocalizationOptions()
+        .SetDefaultCulture("en")
+        .AddSupportedCultures("ru", "en")
+        .AddSupportedUICultures("ru", "en");
+
+    localizationOptions.RequestCultureProviders.Insert(0,
+        new CookieRequestCultureProvider { CookieName = "Schedia.Culture" });
+
+    app.UseRequestLocalization(localizationOptions);
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.UseAntiforgery();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseResponseCompression();
+    }
+
+    app.MapControllers();
+    app.MapAuthEndpoints();
+
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode()
+        .AddAdditionalAssemblies([.. BlazorAssembliesRegistry.Assemblies]);
+
+    await app.RunAsync();
+}
+catch (Exception ex)
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-    app.UseHttpsRedirection();
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-else
+finally
 {
-    IdentityModelEventSource.ShowPII = true;
+    await Log.CloseAndFlushAsync();
 }
-
-app.UseDeveloperExceptionPage();
-app.UseStaticFiles();
-app.UseRouting();
-
-var localizationOptions = new RequestLocalizationOptions()
-    .SetDefaultCulture("en")
-    .AddSupportedCultures("ru", "en")
-    .AddSupportedUICultures("ru", "en");
-
-localizationOptions.RequestCultureProviders.Insert(0,
-    new CookieRequestCultureProvider { CookieName = "Schedia.Culture" });
-
-app.UseRequestLocalization(localizationOptions);
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseAntiforgery();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseResponseCompression();
-}
-
-app.MapControllers();
-app.MapAuthEndpoints();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddAdditionalAssemblies([.. BlazorAssembliesRegistry.Assemblies]);
-
-await app.RunAsync();
